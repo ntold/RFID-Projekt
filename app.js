7eaedb88d499214360a36c37a5707ad88bb38296
ntold <nico.berchtold@me.com>
2019-06-16 21:19:44 +0200

    Added comments

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const config = require('./config/config')
 
 const RFID = require('./models/RFID')
 
+// Importieren der SerialPort Dependency
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
+// Konfigurieren der seriellen Schnittstelle. In diesem Fall ist es
+// die Schnittstelle "COM4"
 const port = new SerialPort('COM4')
+// Parser erstellen, welcher dann die vom Arduino geschickten Daten
+// richtig Formatiert und ausgibt
 const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
 
+// Importieren der Socket.IO Dependency 
+// Konfiguriert auf Port 4000
 const io = require('socket.io')(4000)
 
 const app = express()
@@ -20,23 +27,34 @@ app.use(cors())
 
 require('./routes')(app)
 
+// Verbinden der Datenbank auf dem lokalen Gerät
 mongoose.connect(config.db, { useNewUrlParser: true, useFindAndModify: false })
     .then(() => console.log('Connection was successful'))
     .catch(err => console.error("An error has eccourd:", err))
 
-
+// Die Funktion wird bei jeder Berührung des RFID-Scanners
+// ausgeführt. 
 parser.on('data', (data) => {
+    // Hier wird erstmals nach der soeben erhaltenen RFID
+    // gesucht, um redundante Daten zu vermeiden
     RFID.find({ RFID: data })
         .then(rfid => {
+            // Falls das erhaltene Objekt grösser ist als 1,
+            // so ist die RFID schon in der Datenbank vorhanden
             if (rfid.length >= 1) {
+                // Sendet eine Antwort an das Frontend, dass die
+                // RFID schon vorhanden ist. 
                 io.emit('alreadyTaken', rfid)
             } else {
+                // Erstellt ein Objekt mit der soeben erhaltenen 
+                // RFID
                 const rfid = new RFID({
                     RFID: data,
                     createdDate: new Date().toDateString()
                 })
+                // Speichert das Objekt in der Datenbank
                 rfid.save().then(result => {
-                    //emit new rfid to frontend
+                    // Sendet das erstellte Object an das Frontend
                     io.emit("create", result)
                 }).catch(err => {
                     console.log(err)
--- routes.js
+++ routes.js
@@ -2,39 +2,49 @@ const RFID = require('./models/RFID')
 
 module.exports = (app) => {
 
+    // Abfrage und Ausgaben aller RFID's in der Datenbank
     app.get("/getAll", (req, res, next) => {
         RFID.find((err, RFIDS) => {
             if (err) {
+                // Falls ein Fehler auftritt, diesen 
+                // an den Client schicken.
                 res.status(500).json({
                     error: err
                 })
-            }else{
+            } else {
+                // JSON-Objekt mit allen RFIDS
+                // ausgeben
                 res.status(200).json(RFIDS)
             }
         })
     })
-    
+
+    // Löscht einen bestimmten Datensatz mittels der ID
     app.post("/delete", (req, res) => {
         RFID.deleteOne({ _id: req.body._id }, (err, RFID) => {
             if (err) {
+                // Falls ein Fehler auftritt, diesen 
+                // an den Client schicken.
                 res.status(500).json({
                     error: err
                 })
-            }else{
+            } else {
+                // JSON-Objekt mit allen RFIDS
+                // ausgeben
                 res.status(200).json({
                     message: "RFID removed!"
                 })
             }
         })
     })
-    
+
     app.post("/update", (req, res) => {
         RFID.findByIdAndUpdate(req.body.Id, { Vorname: req.body.Vorname, Nachname: req.body.Nachname }, (err, RFID) => {
             if (err) {
                 res.status(500).json({
                     error: err
                 })
-            }else{
+            } else {
                 res.status(200).json({
                     message: 'RFID Updated!'
                 })
